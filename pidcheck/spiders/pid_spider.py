040d5a02e772d84335825cb7beb1ea0626bd89e6
Richard Hallett <richard@richardhallett.com>
2018-02-02 16:15:42 +0000

    Putting together basic 404 check and extracting schema.org metadata

--- pidcheck/spiders/pid_spider.py
+++ pidcheck/spiders/pid_spider.py
@@ -12,4 +12,50 @@ class PidSpider(scrapy.Spider):
     )
 
     def parse(self, response):
-        yield {}
+        link_result = {
+            'url': response.url,
+            'checked_date': datetime.now(),
+            'quality_score': None,
+            'problems': []
+        }
+
+        problems = []
+        # Check HTTP Status codes for possible problems
+        problems += self.check_http_status(response.status)
+
+        # Extract Schema.org json ld
+        extractor = JsonLdExtractor()
+        schema = extractor.extract(response.body_as_unicode(), response.url)
+
+        # Check schema
+        problems += self.check_schema(schema)
+
+
+        # Add details to the link result
+        link_result['problems'] = problems
+
+        self.log('hit %s' % response.url)
+        yield link_result
+
+    def check_http_status(self, status):
+        problems = []
+
+        # Check for regular 404
+        if status == 404:
+            problems.append("Http 404, not found")
+
+        return problems
+
+    def check_schema(self, schema):
+        problems = []
+
+        if schema:
+            # Look for a PID ID
+            pid = schema[0].get('@id')
+
+            if not pid:
+                problems.append("PID Missing in JsonLD schema.org metadata")
+        else:
+            problems.append("Missing embedded schema.org metadata")
+
+        return problems
