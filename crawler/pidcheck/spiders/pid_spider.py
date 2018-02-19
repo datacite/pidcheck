9dc4bfa80beed6d1182878238db85689b01af663
Richard Hallett <richard@richardhallett.com>
2018-02-19 18:20:48 +0000

    For local development useful to have a simple non redis version. Overrode default settings for JL file based one rather than use redis.

--- crawler/pidcheck/spiders/pid_spider.py
+++ crawler/pidcheck/spiders/pid_spider.py
@@ -67,6 +67,12 @@ class PidJLSpider(PidMixin, scrapy.Spider):
     name = "pidcheck-jl"
     url_file = 'urls.jl'
 
+    custom_settings = {
+        'SCHEDULER': 'scrapy.core.scheduler.Scheduler',
+        'ITEM_PIPELINES': {'pidcheck.pipelines.PIDMetadataIDPipeline': 300},
+        'DUPEFILTER_CLASS': 'scrapy.dupefilters.RFPDupeFilter',
+    }
+
     def start_requests(self):
         with open(self.url_file) as f:
             for jl in f:
