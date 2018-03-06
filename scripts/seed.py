d2c0138c6a9375478f78ab940c862b3da63ccc78
Richard Hallett <richard@richardhallett.com>
2018-03-06 14:23:23 +0000

    Seed from CSV files

--- scripts/seed.py
+++ scripts/seed.py
@@ -3,24 +3,34 @@ import sys
 import os
 import json
 import logging
+import csv
 
 import pidcheck
 
 # Configure basic logging
 logging.basicConfig(level=logging.INFO)
 
-def parse_jsonl(filename):
-    """Parse jsonl file format"""
+def seed_jsonl(filename):
+    """Seed from jsonl file format"""
     with open(filename) as f:
         for jl in f:
             url = json.loads(jl)
             pidcheck.push_pid(url['pid'], url['url'])
 
-def load_from_file(filename):
-    """Load seed urls from a filename"""
-    if filename.endswith('.jsonl'):
-        parse_jsonl(filename)
+def seed_csv(filename):
+    """Seed from csv file"""
+    with open(filename) as f:
+        r = csv.DictReader(f, ['pid', 'url'])
+        for row in r:
+            pidcheck.push_pid(row['pid'], row['url'])
 
 if __name__ == '__main__':
     logging.info("Starting seeding")
-    load_from_file(sys.argv[1])
+
+    filename = sys.argv[1]
+    if filename.endswith('.csv'):
+        seed_csv(filename)
+    elif filename.endswith('.jsonl'):
+        seed_jsonl(filename)
+    else:
+        raise Exception("No known handling for extension")
