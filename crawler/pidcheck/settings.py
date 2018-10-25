f9658e9f51bbb2d8e2371e9ec044c77eb993f134
Richard Hallett <richard@richardhallett.com>
2018-10-25 16:46:40 +0200

    Lower max retry to 3

--- crawler/pidcheck/settings.py
+++ crawler/pidcheck/settings.py
@@ -173,5 +173,6 @@ REDIS_PORT = int(os.getenv('REDIS_PORT', 6379))
 # Use other encoding than utf-8 for redis.
 #REDIS_ENCODING = 'latin1'
 
-RETRY_TIMES = 5
+RETRY_ENABLED = True
+RETRY_TIMES = 3
 RETRY_HTTP_CODES = [500, 503, 504, 400, 408, 404]
